349c3e7d53d624741ee2668124819eac9eab7559
Amrutha <amruthapa051@gmail.com>
2021-10-27 20:12:02 +0530

    largestnum

---
+++ largest.js
@@ -0,0 +1,15 @@
+var num1=10,num2=20,num3=15;
+
+
+if((num1>=num2) && (num1>=num3))
+{
+    console.log("largest number is " ,num1);
+}
+else if((num2>=num1) && (num2>=num3))
+{
+    console.log("largest number is " ,num2);
+}
+else
+{
+    console.log("largest number is " ,num3);
+}
